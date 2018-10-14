a8ecba1f2fd079fd91c11d4b7c694e9fcc710990
rum2mojito <wead3105@sjtu.edu.cn>
2018-10-14 20:25:23 +0800

    add DataDao class

---
+++ server/README.md
@@ -0,0 +1,8 @@
+# Database models
+
+```
+1. id
+2. data_type
+3. data
+4. create_time
+```
--- server/cloud/models.py
+++ server/cloud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class DataDao(models.Model):
+  id = models.IntegerField(auto_now_add=True)
+  create_time = models.TimeField()
+  data_type = models.CharField()
+  data = models.CharField()
+
+  def __str__(self):
+    return self.title
